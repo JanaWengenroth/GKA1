585d6478d8fa7910dccb2e08195d1cd39679e67a
TorbenHaug <claustorbenhaug@hotmail.de>
2014-11-22 20:16:06 +0100

    added some help functions to FlowBaseClass

--- Project/src/GKA/Graph/FordFulkerson.java
+++ Project/src/GKA/Graph/FordFulkerson.java
@@ -1,61 +1,13 @@
 package GKA.Graph;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class FordFulkerson {
+public class FordFulkerson extends FlowBase {
 	
-	private GKAGraph graph;
-	private Matrix<String, Double> maxFlows;
-	private Matrix<String, Double> currentFlows;
-
 	public FordFulkerson(GKAGraph graph){
 		init(graph);
-	}
-	private void init(GKAGraph graph){
-		this.graph = graph;
-		Set<String> vertexes = graph.getjGraph().vertexSet();
-		this.currentFlows = new Matrix<>(vertexes, vertexes);
-		this.maxFlows = generateMaxFlows();
-	}
-	
-	private Matrix<String, Double> generateMaxFlows(){
-		Set<String> vertexes = graph.getjGraph().vertexSet();
-		Matrix<String, Double> flows = new Matrix<>(vertexes, vertexes);
-		for(String row: vertexes){
-			for(String column: vertexes){
-				double flow = 0.0;
-				for(GKAEdge edge: getAccessibleEdgesBetween(row, column)){
-					flow += edge.getWeight();
-				}
-				flows.put(row, column, flow);
-			}
-		}
-		return flows;
-	}
-	
-	private Set<GKAEdge> getAccessibleEdgesBetween(String source, String target){
-		Set<GKAEdge> edges = new HashSet<>(graph.getjGraph().getAllEdges(source, target));
-		Set<GKAEdge> notGoable = new HashSet<>();
-		for (GKAEdge edge : edges) {
-			if(!isEdgeAccessible(edge, source)){
-				notGoable.add(edge);
-			}
-		}
-		edges.removeAll(notGoable);
-		return edges;
-	}
-
-	private boolean isEdgeAccessible(GKAEdge edge, String source){
-		if(edge.getSource().equals(source)){
-			return true;
-		}else if(!graph.isDirected() && edge.getTarget().equals(source)){
-			return true;
-		}else{
-			return false;
-		}
 	}
 	public double maxFlow(String source, String sink){
 		clearFlows();
@@ -74,14 +26,6 @@ public class FordFulkerson {
 			}while(returnedFlow != 0.0);
 			return maxFlow;
 		}
-	}
-	private void clearFlows() {
-		for(String row: currentFlows.getRows()){
-			for(String column: currentFlows.getColumns()){
-				currentFlows.put(row, column, 0.0);
-			}
-		}
-		
 	}
 	private double maxFlow_(ArrayList<String> alreadyReached, String source, String sink, double maxFlow){
 		if (source.equals(sink)){ 
@@ -129,26 +73,5 @@ public class FordFulkerson {
 			}
 		}
 		return 0.0;
-	}
-	private Set<String> forwardVertexesofSources(String source) {
-		Set<String> vertexes = new HashSet<>();
-		for (String vertex: maxFlows.getColumns()){
-			if (currentFlows.get(source, vertex) < maxFlows.get(source, vertex)){
-				vertexes.add(vertex);
-			}
-		}
-		return vertexes;
-	}
-	private Set<String> reverseVertexesofSources(String source){
-		Set<String> vertexes = new HashSet<>();
-		for (String vertex: maxFlows.getColumns()){
-			if (currentFlows.get(vertex, source) > 0.0){
-				vertexes.add(vertex);
-			}
-		}
-		return vertexes;
-	}
-	private double getPossibleFlowBetween(String source, String sink){
-		return maxFlows.get(source, sink) - currentFlows.get(source, sink);
 	}
 }
